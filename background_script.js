202b104f47a092f6a23fa38ee775e6eb0e0dd5ee
cyberst <salihoglual@itu.edu.tr>
2020-04-16 00:39:04 +0300

    Initial Commit

---
+++ background_script.js
@@ -0,0 +1,36 @@
+browser.browserAction.onClicked.addListener(GoToSoundTab);
+
+function GoToSoundTab() {
+    browser.windows.getLastFocused({populate:true}).then(currentWindow => {
+        if(!GoToSoundTabInWindow(currentWindow, true))
+        {
+            browser.windows.getAll({populate:true}).then(allWindows => {
+ 
+                if(!(allWindows.filter(window => window.id !== currentWindow.id)
+                .some(window => GoToSoundTabInWindow(window, false))))
+                {
+                    GoToSoundTabInWindow(currentWindow, false);
+                }
+            })
+        }
+    })
+}
+function GoToSoundTabInWindow(window, isCurrentWindow){
+    audibleTabs = window.tabs.filter(tab => tab.audible);
+    if(isCurrentWindow){
+        currentTab = window.tabs.find(tab => tab.active);
+        audibleTabs = audibleTabs.filter(tab => tab.index > currentTab.index);    
+    }
+    if(audibleTabs.length > 0){
+        browser.tabs.update(audibleTabs[0].id,{active:true})
+        if(!isCurrentWindow){
+            setTimeout(() => 
+                browser.windows.update(window.id, {focused:true})
+            , 150);
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+}
